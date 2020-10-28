5afab04d7d27ffea6d98cd38a695e56b216a0a1e
Justin Z <contactjustinzamora@gmail.com>
2020-10-27 19:18:44 -0700

    User model added

--- models.py
+++ models.py
@@ -7,6 +7,11 @@ bcrypt = Bcrypt()
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column{db.String(length=20), unique=True}
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(length=50), unique=True, nullable=False)
+    first_name = db.Column(db.Text(length=30), nullable=False)
+    last_name = db.Column(db.Text(length=30), nullable=False)
 
 def connect_db(app):
     db.app = app
